72166f346c7752e8967847d7ac09435c04e8b3eb
Steven Yu <stevenyu1106@gmail.com>
2020-04-02 23:28:29 +0800

    good version

--- app.py
+++ app.py
@@ -50,20 +50,18 @@ def handle_message(event):
         )
 
         line_bot_api.reply_message(
-        event.reply_token,
-        sticker_message)
+            event.reply_token,
+            sticker_message)
         return
 
     if msg in ['hi', 'Hi']:
-        r = 'hi'
+        r = '嗨'
     elif msg == '你吃飯了嗎':
         r = '還沒'
     elif msg == '你是誰':
         r = '我是機器人'
     elif '訂位' in msg:
         r = '您想訂位, 是嗎?'
-
-
 
 
 if __name__ == "__main__":
