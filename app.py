3c4d92f2d2579d6f31d08cecc3cd249898c7841e
Steven Yu <stevenyu1106@gmail.com>
2020-04-02 23:34:19 +0800

    add sticker

--- app.py
+++ app.py
@@ -63,6 +63,10 @@ def handle_message(event):
     elif '訂位' in msg:
         r = '您想訂位, 是嗎?'
 
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
+
 
 if __name__ == "__main__":
     app.run()
